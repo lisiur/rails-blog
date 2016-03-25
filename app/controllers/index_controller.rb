a42c05382997e7737b6c3bedb34789a25c255bda
daikanshan <daikanshan@163.com>
2016-03-25 17:08:42 +0800

    logo

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,13 @@
 class IndexController < ApplicationController
   before_action :set_all_article_types
+  before_action :set_login_author
 
   private
   def set_all_article_types
     @all_article_types = Admin::ArticleType.all
+  end
+
+  def set_login_author
+    @login_author = Admin::Author.find_by_id(session[:author_id]) if session[:author_id]
   end
 end
