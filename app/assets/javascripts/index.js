f3791e652766cf18a6ef175c31fef9e552c2816c
daikanshan <daikanshan@163.com>
2016-04-03 19:38:52 +0800

    修改分页被footer盖住bug&&使用session优化查询weather次数

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,6 +2,7 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function(){
+
   if($(".time-line")[0] != undefined){
     left = $(".time-line")[0].offsetLeft;
     width = $(".time-line").width();
@@ -14,12 +15,14 @@ $(document).ready(function(){
         $(".line").css("left",result);
     });
   }
+
   footer = $("#footer")[0];
   if((footer.offsetTop + footer.clientHeight) < window.outerHeight){
     $(footer).css("position" , "fixed");
   }
+
   // baiduapi
-  api_key  = "732b40aadc7409a1349780c6fb7a55f3";
+  // api_key  = "732b40aadc7409a1349780c6fb7a55f3";
   // 获取ip所在地
   // http_url = "http://apis.baidu.com/apistore/iplookupservice/iplookup";
   // http_arg = {ip:"117.89.35.58"};
@@ -33,31 +36,31 @@ $(document).ready(function(){
   //   }
   // })
   // 获取天气
-  http_url = "http://apis.baidu.com/apistore/weatherservice/citylist";
-  http_arg = {cityname:"江宁"};
-  arg = '';
-  $.ajax({
-    url:http_url,
-    data:http_arg,
-    type:'GET',
-    dataType:'json',
-    beforeSend: function(xhr){xhr.setRequestHeader('apikey',api_key );},
-
-    success:function(data){
-      http_url = "http://apis.baidu.com/apistore/weatherservice/cityid";
-      http_arg = {cityid:data["retData"][0]["area_id"]}
-      $.ajax({
-        url:http_url,
-        data:http_arg,
-        type:'GET',
-        dataType:'json',
-        beforeSend: function(xhr){xhr.setRequestHeader('apikey',api_key );},
-        success:function(data){
-          if(data["retData"]["weather"]=="多云"){
-            
-          }
-        }
-      })
-    }
-  })
+  // http_url = "http://apis.baidu.com/apistore/weatherservice/citylist";
+  // http_arg = {cityname:"江宁"};
+  // arg = '';
+  // $.ajax({
+  //   url:http_url,
+  //   data:http_arg,
+  //   type:'GET',
+  //   dataType:'json',
+  //   beforeSend: function(xhr){xhr.setRequestHeader('apikey',api_key );},
+  //
+  //   success:function(data){
+  //     http_url = "http://apis.baidu.com/apistore/weatherservice/cityid";
+  //     http_arg = {cityid:data["retData"][0]["area_id"]}
+  //     $.ajax({
+  //       url:http_url,
+  //       data:http_arg,
+  //       type:'GET',
+  //       dataType:'json',
+  //       beforeSend: function(xhr){xhr.setRequestHeader('apikey',api_key );},
+  //       success:function(data){
+  //         if(data["retData"]["weather"]=="多云"){
+  //
+  //         }
+  //       }
+  //     })
+  //   }
+  // })
 })
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -15,58 +15,60 @@ class IndexController < ApplicationController
   end
 
   def set_webgl
-    api_key  = "732b40aadc7409a1349780c6fb7a55f3"
-    uri = 'http://apis.baidu.com'
-    ip = request.remote_ip
+    unless session[:current_weather]
+      api_key  = "732b40aadc7409a1349780c6fb7a55f3"
+      uri = 'http://apis.baidu.com'
+      ip = request.remote_ip
 
-    conn = Faraday.new(:url => uri ) do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
+      conn = Faraday.new(:url => uri ) do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
 
-    # ip = "112.80.186.74"
-    response_city = conn.get do |req|
-      req.url '/apistore/iplookupservice/iplookup',:ip => ip
-      req.headers['apikey'] = api_key
-      req.options.timeout = 5           # open/read timeout in seconds
-      req.options.open_timeout = 2      # connection open timeout in seconds
-    end
-    begin
-      city_name = JSON.parse(response_city.body)['retData']["city"]
-      # district_name = JSON.parse(response_city.body)['retData']["district"]
-    rescue
-      city_name = ''
-    end
+      # ip = "112.80.186.74"
+      response_city = conn.get do |req|
+        req.url '/apistore/iplookupservice/iplookup',:ip => ip
+        req.headers['apikey'] = api_key
+        req.options.timeout = 5           # open/read timeout in seconds
+        req.options.open_timeout = 2      # connection open timeout in seconds
+      end
+      begin
+        city_name = JSON.parse(response_city.body)['retData']["city"]
+        # district_name = JSON.parse(response_city.body)['retData']["district"]
+      rescue
+        city_name = ''
+      end
 
-    # response_city_list = conn.get do |req|
-    #   req.url '/apistore/weatherservice/citylist',:cityname => city_name
-    #   req.headers['apikey'] = api_key
-    #   req.options.timeout = 5           # open/read timeout in seconds
-    #   req.options.open_timeout = 2      # connection open timeout in seconds
-    # end
-    # city_list = JSON.parse(response_city_list.body)["retData"]
+      # response_city_list = conn.get do |req|
+      #   req.url '/apistore/weatherservice/citylist',:cityname => city_name
+      #   req.headers['apikey'] = api_key
+      #   req.options.timeout = 5           # open/read timeout in seconds
+      #   req.options.open_timeout = 2      # connection open timeout in seconds
+      # end
+      # city_list = JSON.parse(response_city_list.body)["retData"]
 
-    response_weather = conn.get do |req|
-      req.url '/apistore/weatherservice/cityname',:cityname => city_name
-      req.headers['apikey'] = api_key
-      req.options.timeout = 5           # open/read timeout in seconds
-      req.options.open_timeout = 2      # connection open timeout in seconds
-    end
-    begin
-      weather = JSON.parse(response_weather.body)["retData"]["weather"]
-    rescue
-      weather = nil
-    end
+      response_weather = conn.get do |req|
+        req.url '/apistore/weatherservice/cityname',:cityname => city_name
+        req.headers['apikey'] = api_key
+        req.options.timeout = 5           # open/read timeout in seconds
+        req.options.open_timeout = 2      # connection open timeout in seconds
+      end
+      begin
+        weather = JSON.parse(response_weather.body)["retData"]["weather"]
+      rescue
+        weather = nil
+      end
 
-    @true_ip = ip
-    @true_city = city_name
-    @true_weather = weather
+      @true_ip = ip
+      @true_city = city_name
+      @true_weather = weather
 
-    if !weather.nil?
-      @weather = 'rain' if weather.include?("雨")
-      @weather = 'snow' if weather.include?("雪")
+      if !weather.nil?
+        session[:current_weather] = 'rain' if weather.include?("雨")
+        session[:current_weather] = 'snow' if weather.include?("雪")
+      end
+      session[:current_weather] = 'snow' if session[:current_weather].nil?
     end
-    @weather = 'snow' if @weather.nil?
   end
 end
